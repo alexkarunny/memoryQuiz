ee9bb46b602df75ef032db7bcc59786608b63608
Iniok <ilidanilo@mail.ru>
2023-11-12 13:52:08 +0300

    add polymorphic button

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
+import './styles/index.scss'
 
 import { App } from '@/App'
 import ReactDOM from 'react-dom/client'
+import React from 'react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
