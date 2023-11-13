64fd8207faa06b61e9ed31d78286b62e53fb0d18
Iniok <ilidanilo@mail.ru>
2023-11-13 23:03:20 +0300

    Images Components

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import '@fontsource/roboto/400.css'
-import '@fontsource/roboto/700.css'
-import './styles/index.scss'
+import React from 'react'
 
 import { App } from '@/App'
 import ReactDOM from 'react-dom/client'
-import React from 'react'
+
+import './styles/index.scss'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
