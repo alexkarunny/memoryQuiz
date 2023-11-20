ab521034bb9ec93d70db2dcca63eef35748df365
Yury Daszukiewicz <ydaszukiewicz@gmail.com>
2023-11-20 10:13:04 +0300

    feat: add radio-group component with radix

---
+++ src/components/ui/radio-group/radio-item/radio-item.tsx
@@ -0,0 +1,25 @@
+import { ComponentPropsWithoutRef } from 'react'
+
+import { Typography } from '@/components/ui/typography'
+import { Indicator, Item } from '@radix-ui/react-radio-group'
+
+import s from './radio-item.module.scss'
+
+export type RadioItems = {
+  label: string
+} & ComponentPropsWithoutRef<typeof Item>
+
+export const RadioItem = (props: RadioItems) => {
+  const { disabled, label, value } = props
+
+  return (
+    <div className={s.container}>
+      <Typography as={'label'} className={disabled ? s.label : ''} variant={'body2'}>
+        <Item className={s.RadioGroupItem} disabled={disabled} value={value}>
+          <Indicator className={s.RadioGroupIndicator} />
+        </Item>
+        {label}
+      </Typography>
+    </div>
+  )
+}
