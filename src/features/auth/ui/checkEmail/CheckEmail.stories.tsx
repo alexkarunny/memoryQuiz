4cb337ab93063b1401e7b39c26818b05dc213314
Soucron <ilidanilo@mail.ru>
2023-11-25 14:00:55 +0300

    checkEmail form

---
+++ src/features/auth/ui/checkEmail/CheckEmail.stories.tsx
@@ -0,0 +1,13 @@
+import { CheckEmail } from '@/features/auth/ui/checkEmail/checkEmail'
+import { Meta, StoryObj } from '@storybook/react'
+
+const meta = {
+  component: CheckEmail,
+  tags: ['autodocs'],
+  title: 'Auth/CheckMail',
+} satisfies Meta<typeof CheckEmail>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {}
---
+++ src/features/auth/ui/checkEmail/checkEmail.tsx
@@ -0,0 +1,31 @@
+import { Email } from '@/assets'
+import { Button } from '@/components/ui/button'
+import { Card } from '@/components/ui/card'
+import { Typography } from '@/components/ui/typography'
+import { clsx } from 'clsx'
+
+import s from '@/features/auth/ui/checkEmail/CheckEmail.module.scss'
+
+export const CheckEmail = () => {
+  const classNames = {
+    checkMailLabel: clsx(s.checkMailLabel),
+    confirmButton: clsx(s.confirmButton),
+    container: clsx(s.container),
+    instructionsLabel: clsx(s.instructionsLabel),
+  }
+
+  return (
+    <Card className={classNames.container}>
+      <Typography as={'h1'} className={classNames.checkMailLabel} variant={'large'}>
+        Check Email
+      </Typography>
+      <Email z-index={100} />
+      <Typography className={classNames.instructionsLabel} variant={'body2'}>
+        We’ve sent an Email with instructions to example@mail.com
+      </Typography>
+      <Button className={classNames.confirmButton} fullWidth>
+        Back to sign In
+      </Button>
+    </Card>
+  )
+}
