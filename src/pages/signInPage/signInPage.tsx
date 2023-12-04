99c9dcdd3691a296be77e432b8974554fb6172d0
Soucron <ilidanilo@mail.ru>
2023-12-04 22:09:27 +0300

    SignInPage

--- src/pages/signInPage/signInPage.tsx
+++ src/pages/signInPage/signInPage.tsx
@@ -1,12 +1,22 @@
+import { Navigate } from 'react-router-dom'
+
 import { Page } from '@/components/ui/page'
-import { SignInDataType, useLoginMutation } from '@/features/auth/api'
+import { SignInDataType, useLoginMutation, useMeQuery } from '@/features/auth/api'
 import { SignInForm } from '@/features/auth/ui/signInForm'
 
 export const SignInPage = () => {
   const [signIn] = useLoginMutation()
 
+  const { isError } = useMeQuery()
+
+  const isAuth = !isError
+
   const signInHandler = ({ email, password, rememberMe }: SignInDataType) => {
     signIn({ email, password, rememberMe })
+  }
+
+  if (isAuth) {
+    return <Navigate replace to={'/'} />
   }
 
   return (
