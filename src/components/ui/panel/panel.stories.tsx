f26c6d77956c4f7a881ea01f70fb3cb5cdd5b745
Iniok <ilidanilo@mail.ru>
2023-12-19 23:53:16 +0300

    panel component

---
+++ src/components/ui/panel/panel.stories.tsx
@@ -0,0 +1,12 @@
+import { Meta, StoryObj } from '@storybook/react'
+import {Panel} from '@/components/ui/panel/panel.tsx';
+
+const meta = {
+    component: Panel,
+    tags: ['autodocs'],
+    title: 'Components/Page',
+} satisfies Meta<typeof Panel>
+
+export default meta
+type Story = StoryObj<typeof meta>
+export const Default: Story = {}
--- src/components/ui/panel/panel.tsx
+++ src/components/ui/panel/panel.tsx
@@ -1,5 +1,17 @@
-export const Panel = () => {
-  const classNames = {}
+import clsx from 'clsx'
+import s from './panel.module.scss'
+import {Input} from '@/components/ui/input';
+import {Slider} from '@/components/ui/slider';
+import {Button} from '@/components/ui/button';
 
-  return <div></div>
+export const Panel = () => {
+  const classNames = {
+    root:clsx(s.root)
+  }
+
+  return <div className={classNames.root}>
+    <Input placeholder={'Search'}/>
+    <Slider/>
+    <Button variant={'secondary'}/>
+  </div>
 }
