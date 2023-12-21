0713ad0545270856d56eb4ef70e20e4d7089ab06
Soucron <ilidanilo@mail.ru>
2023-12-22 00:04:59 +0300

    panel component

--- src/components/ui/panel/panel.tsx
+++ src/components/ui/panel/panel.tsx
@@ -1,17 +1,37 @@
+import { ThrashIcon } from '@/assets'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Slider } from '@/components/ui/slider'
+import { TabSwitcher } from '@/components/ui/tab-switcher'
+import { Typography } from '@/components/ui/typography'
 import clsx from 'clsx'
+
 import s from './panel.module.scss'
-import {Input} from '@/components/ui/input';
-import {Slider} from '@/components/ui/slider';
-import {Button} from '@/components/ui/button';
 
 export const Panel = () => {
   const classNames = {
-    root:clsx(s.root)
+    button: clsx(s.button),
+    input: clsx(s.input),
+    root: clsx(s.root),
+    tabs: clsx(s.tabs),
   }
 
-  return <div className={classNames.root}>
-    <Input placeholder={'Search'}/>
-    <Slider/>
-    <Button variant={'secondary'}/>
-  </div>
+  const tabs = [
+    { label: 'My Cards', value: 'My' },
+    { label: 'All Cards', value: 'All' },
+  ]
+
+  return (
+    <div className={classNames.root}>
+      <Input className={classNames.input} placeholder={'Search'} variant={'search'} />
+      <TabSwitcher className={classNames.tabs} label={'Show packs cards'} tabs={tabs} />
+      <Slider label={'Number of cards'} />
+      <Button className={classNames.button} variant={'secondary'}>
+        <ThrashIcon size={16} />
+        <Typography as={'span'} variant={'subtitle2'}>
+          Clear Filter
+        </Typography>
+      </Button>
+    </div>
+  )
 }
