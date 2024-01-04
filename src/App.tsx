fc0310888ac61c05b1af7f2e89ef414176111cc2
Soucron <ilidanilo@mail.ru>
2024-01-04 23:50:28 +0300

    PersonalInformationPage start

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Provider } from 'react-redux'
 
 import { Router } from '@/common/routes/router'
+import { PersonalInformation } from '@/features/profile/ui/personalInformation'
 import { store } from '@/services/store'
 
 export function App() {
   return (
     <Provider store={store}>
       <Router />
+      <PersonalInformation />
     </Provider>
   )
 }
---
+++ src/pages/personalInformationPage/personalInformationPage.tsx
@@ -0,0 +1,10 @@
+import { Page } from '@/components/ui/page'
+import { PersonalInformation } from '@/features/profile/ui/personalInformation'
+
+export const UpdateProfilePage = () => {
+  return (
+    <Page>
+      <PersonalInformation />
+    </Page>
+  )
+}
